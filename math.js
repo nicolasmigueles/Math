532337748d84e478d144149b9de807ee6e537051
nicolasmigueles <nicolasmfotografo@gmail.com>
2017-03-26 12:27:40 -0300

    v2 26-3-17
    falta al poner 2 numeros seguidos que no los separe sino que los ponga
    en el array nums asi como estan.
    !isNaN and i+1 == !isNaN => slice[i+1] y push (i+(i+1))(COMO STRING);

--- math.js
+++ math.js
@@ -3,20 +3,36 @@
  */
 
 // Variables
+
+String.prototype.replaceAll = function(search, replacement) {
+    var target = this;
+    return target.replace(new RegExp(search, 'g'), replacement);
+};
  var term = new Array;
  var vars = new Array;
  var nums = new Array;
  var termindep = new Array;
+ var newterm = new Array;
  var sumaterminosindependientes = 0;
+ var final = null;
+ var finalvars = new Array;
+ var negative = false;
 // Funciones
  function Terminos(ecuacion){
    term = [];
-   var ecu = ecuacion.replace("-", "+-");
+   var ecu = ecuacion.replaceAll("-", "+-");
+   if (ecu[0] == "+") {
+     //correccion de algoritmo por si la cadena empieza con -
+     //es reemplazado innecesariamente y sepera en un termino que no existe.
+     ecu = ecu.substr(1);
+   }
+   console.log(ecu);
    var t = ecu.split("+");
    for (var i in t) {
      term.push(t[i]);
    }
-   console.log(term);
+   //console.log(term);
+   console.info("Se analiza la operación: "+ecuacion);
    return term;
  }
  function Variables(termino,numterm){
@@ -34,31 +50,33 @@
    }
    var coef = 1;
    for (var i in nums) {
-
      coef = coef * nums[i];
    }
    coef == 1 ? "" : coef;
    var response = null;
    if (vars.length == 0) {
-     if (nums.length > 1) {
-       nums.slice(i);
-       sumaterminosindependientes = nums[i];
-     }else{
-       final = parseInt(coef) + parseInt(sumaterminosindependientes);
-       response = "El termino "+numterm+" es independiente: "+final;
-     }
-
+      go = nums[0];
+       for (var o in nums) {
+         if (nums[0] == -1) {
+           go = nums[1]* (-1);
+         }
+       }
+       termindep.push(parseFloat(go));
    }else{
      if (coef == 1) {
        response = "El termino "+numterm+" tiene "+ vars.length +" variable/s; El termino queda: '"+vars.join("")+"'";
+       if (!finalvars.includes(vars.join(""))) {
+         finalvars.push(vars.join(""));
+       }
      }else{
        response = "El termino "+numterm+" tiene "+ vars.length +" variable/s; y su coeficiente es "+coef+" , el termino queda: '"+coef+vars.join("")+"'";
+       if (!finalvars.includes(coef+vars.join(""))) {
+         finalvars.push(coef+vars.join(""));
+       }
      }
-
    }
    vars = [];
    nums = [];
-
    return response;
  }
  function AnalizarTerminos(){
@@ -69,7 +87,36 @@
        console.log(Variables(term[t],numtermino));
      }
    }
+   if (finalvars  != null) {
+     newterm.push(finalvars.join("+"));
+   }
+   // Se guarda el termino indep
+   final = termindep.reduce(function(valorAnterior, valorActual, indice, vector){
+     var responsebro = valorAnterior + valorActual;
+       if (responsebro == 0) {
+         return null;
+       }else{
+       return responsebro;
+       }
+   });
+   if (final != null) {
+     newterm.push(final);
+   }else{
+     console.log("El termino independiente se elimina ya que da 0.");
+   }
+   var splitnewterm = newterm.join("+");
+   console.info(splitnewterm);
  }
+ function Operar(x,y){
+   // opera dos numeros.
+   x == null ? x = 0 : x;
+   y == null ? y = 0 : y;
+   return parseFloat(x) + parseFloat(y);
+ }
+
+
 // Prueba
-Terminos("x5g4-7+4");
+Terminos("-3f-2x-7");
 AnalizarTerminos();
+
+// falta al poner dos numeros juntos que no los multiplique.
